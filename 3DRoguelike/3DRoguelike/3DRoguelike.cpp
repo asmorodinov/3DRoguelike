133f700462b3580252d2a633b419b84a3f8b28aa
Alexander Smorodinov <asmorodinov66@gmail.com>
2022-07-02 01:38:55 +0300

    added vcpkg and spdlog

--- 3DRoguelike/3DRoguelike/3DRoguelike.cpp
+++ 3DRoguelike/3DRoguelike/3DRoguelike.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <concepts>
 
+#include <spdlog/spdlog.h>
+
 template <typename T>
 requires std::integral<T> || std::floating_point<T>
 constexpr double Average(std::vector<T> const& vec) {
@@ -14,4 +16,7 @@ int main() {
     std::vector ints{1, 2, 3, 4, 5};
 
     std::cout << Average(ints) << '\n';
+
+    spdlog::info("Welcome to spdlog!");
+    spdlog::error("Some error message with arg: {}", 1);
 }
