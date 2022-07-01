eb10c381b49917fb771a44e67ae78e88b181d274
Alexander Smorodinov <asmorodinov66@gmail.com>
2022-07-01 23:20:41 +0300

    clang format config file added

--- 3DRoguelike/3DRoguelike/3DRoguelike.cpp
+++ 3DRoguelike/3DRoguelike/3DRoguelike.cpp
@@ -4,13 +4,14 @@
 #include <concepts>
 
 template <typename T>
-    requires std::integral<T> || std::floating_point<T>
+requires std::integral<T> || std::floating_point<T>
 constexpr double Average(std::vector<T> const& vec) {
     const double sum = std::accumulate(vec.begin(), vec.end(), 0.0);
     return sum / vec.size();
 }
 
 int main() {
-    std::vector ints{ 1, 2, 3, 4, 5 };
+    std::vector ints{1, 2, 3, 4, 5};
+
     std::cout << Average(ints) << '\n';
 }
