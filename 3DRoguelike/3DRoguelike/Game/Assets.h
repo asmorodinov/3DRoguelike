ab18a0fba75ccf41c25d0d327710b8d30289fc8c
Alexander Smorodinov <asmorodinov66@gmail.com>
2022-08-20 15:20:43 +0300

    changed some methods to be private

--- 3DRoguelike/3DRoguelike/Game/Assets.h
+++ 3DRoguelike/3DRoguelike/Game/Assets.h
@@ -25,8 +25,6 @@ class Assets {
     static Shader& GetShader(const std::string& vertexShader, const std::string& fragmentShader);
     static const Texture& GetTexture(const std::string& name);
     static const ModelData& GetModelData(const std::string& name);
-    static const YAML::Node& GetYAMLFile(const std::string& name);
-    static const YAML::Node& GetConfig();
 
     template <typename T>
     static T GetConfigParameter(const std::string& name) {
@@ -36,12 +34,15 @@ class Assets {
     static const bool HasConfigParameter(const std::string& name);
 
  public:
-    glm::mat4 orthogonalProjection;
-    glm::mat4 projection;
-    glm::mat4 view;
+    glm::mat4 orthogonalProjection = glm::mat4();
+    glm::mat4 projection = glm::mat4();
+    glm::mat4 view = glm::mat4();
 
  private:
     Assets() = default;
+
+    static const YAML::Node& GetYAMLFile(const std::string& name);
+    static const YAML::Node& GetConfig();
 
  private:
     std::unordered_map<std::string, Shader> shaders;
